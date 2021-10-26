b6b14f55241e7d9995d9e76a12654bfc22e2ad3c
creamy-dev <greysoh@pm.me>
2021-10-26 19:28:53 -0400

    testing

---
+++ project-kilo/project-kilo.cpp
@@ -0,0 +1,12 @@
+﻿// project-kilo.cpp : Defines the entry point for the application.
+//
+
+#include "project-kilo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
