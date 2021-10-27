478678b4e8d62dcf8f034ebe730f78bdf92a702a
creamy-dev <greysoh@pm.me>
2021-10-26 20:35:28 -0400

    Set up electron & change project name

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# project-kilo
+# Coffi
 code editor inspired by doom emacs, 4coder, dion, and a random image of randy in the video "inverse kinematics"
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!")
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true, // importnat
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: true //for forcing full screen
+    }
+  })
+
+  win.loadFile('./src/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
