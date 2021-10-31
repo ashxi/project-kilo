97bb650ff796330fe8546c7f684b948a6c01a50b
creamy-dev <greysoh@pm.me>
2021-10-31 08:59:06 -0400

    seperate brew api

---
+++ src/brewAPI.js
@@ -0,0 +1,37 @@
+const brew = { location: {
+    replaceHTML: function(data) {
+        document.getElementById("mainWindow").innerHTML = data;
+        
+        const html = new DOMParser().parseFromString(data, 'text/html');
+        let doc = html.getElementsByTagName('script');
+        let title = html.getElementsByTagName('title');
+            
+        for (docs of doc) {
+            eval(docs.innerHTML);
+        }
+
+        for (newTitle of title) {
+            document.title = newTitle.innerHTML;
+        }
+    },
+    replace: function(url) {
+        const data = fs.readFileSync(`${__dirname}/${url}`, {encoding:'utf8', flag:'r'});
+
+        brew.location.replaceHTML(data);
+    },
+    reload: function() {
+        let splitter = window.location.href.split("/");
+    
+        brew.location.replace(splitter[splitter.length - 1]);
+    }
+  },
+  homebrew: {
+      aboutBrew: function() {
+          console.log(`Running Brew ${brew.homebrew.brewVer}, by Bedrock Team.`);
+          return(true);
+      },
+      brewVer: "0.1a"
+  }
+};
+
+exports.brew = brew;
--- src/preload.js
+++ src/preload.js
@@ -1,42 +1,6 @@
 const fs = require("fs");
 const { contextBridge } = require("electron")
-
-let brew = { location: {
-    replaceHTML: function(data) {
-        document.getElementById("mainWindow").innerHTML = data;
-        
-        const html = new DOMParser().parseFromString(data, 'text/html');
-        let doc = html.getElementsByTagName('script');
-        let title = html.getElementsByTagName('title');
-            
-        for (docs of doc) {
-            eval(docs.innerHTML);
-        }
-
-        for (newTitle of title) {
-            document.title = newTitle.innerHTML;
-        }
-    },
-    replace: function(url) {
-        const data = fs.readFileSync(`${__dirname}/${url}`, {encoding:'utf8', flag:'r'});
-
-        brew.location.replaceHTML(data);
-    },
-    reload: function() {
-        let splitter = window.location.href.split("/");
-    
-        brew.location.replace(splitter[splitter.length - 1]);
-    }
-  },
-  homebrew: {
-      aboutBrew: function() {
-          console.log(`Running Brew ${brew.homebrew.brewVer}, by Bedrock Team.`);
-          return(true);
-      },
-      brewVer: "0.1a"
-  }
-};
-
+const brew = require("./brewAPI.js").brew;
 contextBridge.exposeInMainWorld("brew", brew);
 
 async function loadThemes() {
