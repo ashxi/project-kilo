eefe1549c69ce4903b9c830c8c7e44122d6d3272
creamy-dev <56458201+creamy-dev@users.noreply.github.com>
2021-11-20 13:57:22 -0500

    chore: Gives information about the project in UsefulCommit.

--- tooling/usefulcommit/help.js
+++ tooling/usefulcommit/help.js
@@ -1,6 +1,7 @@
 const VERSION = JSON.parse(JSON.stringify(require("./global.json")))["version"]
 
 console.log("UsefulCommit v" + VERSION);
+console.log("Written by Greyson/creamy-dev, for Concrete.")
 console.log("-----------------");
 const command = ["--help", "--commit='commit name'", "--type='type of commit'", "--lint"];
 const func = ["Lists help commands", "Commit name for linting", "Type for linting", "Lints commit and gives you a result"];
