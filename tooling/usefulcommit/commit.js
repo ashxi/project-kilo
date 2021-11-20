a5e0e81c4c94ac8e5ca5c71eb41a2c1fe79363d3
creamy-dev <56458201+creamy-dev@users.noreply.github.com>
2021-11-20 13:07:35 -0500

    feature: Adds punctuation detection and formatting.

--- tooling/usefulcommit/commit.js
+++ tooling/usefulcommit/commit.js
@@ -3,17 +3,33 @@ const VERSION = JSON.parse(JSON.stringify(require("./global.json")))["version"]
 module.exports = async function(commit, typer) {
     const config = JSON.parse(JSON.stringify(require("./config.json")));
     let type = typer.toLowerCase();
-    function genCommit(commit, typer) {
-        let commitStr = commit.charAt(0).toUpperCase() + commit.slice(1);
+    async function genCommit(commitOrig, typer) {
+        let commitStr = commitOrig.charAt(0).toUpperCase() + commitOrig.slice(1);
+        let commit = config.format.replace("TYPE", type).replace("COMMIT_DETAILS", commitStr).replace("VERSION", "UsefulCommit::Lint v" + VERSION);
 
-        console.log(config.format.replace("TYPE", type).replace("COMMIT_DETAILS", commitStr).replace("VERSION", "UsefulCommit::Lint v" + VERSION))
+        let punc = ["!", "."] // Actual things to end your sentence with that DOES make sense.
+        // TODO: Add more punctuation.
+
+        let commitTest = false;
+
+        for await(punctuation of punc) {
+            if (commit.endsWith(punctuation)) {
+                commitTest = true;
+            }
+        }
+
+        if (!commitTest) {
+            commit += ".";
+        }
+
+        return(commit);
     }
 
     if (config.validTypes.includes(type)) {
-        genCommit(commit, typer);
+        console.log(await genCommit(commit, typer));
     } else if (!config.isEnforced) {
         console.log("(UsefulCommit::Lint) [WARN] Your commit type is not a valid type! Ignoring...")
-        genCommit(commit, typer)
+        console.log(await genCommit(commit, typer));
     } else {
         console.log("(UsefulCommit::Lint) [ERROR] Your commit type is not a valid type!")
         process.exit(1);
