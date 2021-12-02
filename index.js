d5a67722b88c3c6bfaed17a17808425da3b40b93
Greyson <56458201+creamy-dev@users.noreply.github.com>
2021-12-02 13:38:53 -0500

    chore: Start documenting code.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain, dialog, Menu, MenuItem } = require('electron'),
       fs = require('fs');
 
+// Sleep function needed for while(true) loops.
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -11,22 +12,30 @@ const BypassHangup = false;
 let mainWindow,
     loaderMain,
     windowInfo;
-
+/*
+* Creates windows, loading screen and main display.
+*/
 function createWindow () {
+    // Auto updates the config, manually.
     async function autoUpdate() {
         while (true) {
             try {
+                // Built string for config.
                 let builtString;
                 
+                // If is maximized, return same height with maximized paramater.
                 if (mainWindow.isMaximized()) {
                     builtString = `{\n  "width": ${windowInfo.width},\n  "height": ${windowInfo.height},\n  "x": ${windowInfo.x},\n  "y": ${windowInfo.y},\n  "isMaximized": true,\n  "isDevMode": ${windowInfo.isDevMode},\n  "bypassConfig":${windowInfo.bypassConfig}\n}`;
                 } else {
                     builtString = `{\n  "width": ${mainWindow.getSize()[0]},\n  "height": ${mainWindow.getSize()[1]},\n  "x": ${mainWindow.getPosition()[0]},\n  "y": ${mainWindow.getPosition()[1]},\n  "isMaximized": ${mainWindow.isMaximized()},\n  "isDevMode": ${windowInfo.isDevMode},\n  "bypassConfig":${windowInfo.bypassConfig}\n}`;
                 }
 
+                // Writes file to local config.json
                 await fs.promises.writeFile('./config.json', builtString);
+                // Updates 'windowInfo' to latest builtString
                 windowInfo = JSON.parse(builtString);
             } catch (e) {
+                // Not *completely* fatal if it fails, although important. This could be caused by the local disk being full.
                 console.warn(e);
             }
 
@@ -35,17 +44,22 @@ function createWindow () {
     }
 
     try {
+        // Reads config, and writes it at initialization.
         let magic = fs.readFileSync("./config.json");
         windowInfo = JSON.parse(magic);
     } catch (err) {
         try {
+            // Saves loaded configuration if it doesn't exist.
             let loadedJSON = `{\n  "width": 800,\n  "height": 600,\n  "x": null,\n  "y": null,\n  "isMaximized": true,\n  "isDevMode": false,\n  "bypassConfig":false\n}`;
             fs.writeFileSync("./config.json", loadedJSON);
             windowInfo = JSON.parse(loadedJSON);
         } catch (e) {
+            // Near fatal error, warns the user via console that there was 2 errors loading the config.
             console.error(`There were 2 errors loading the configuration! ${err}; ${e}`);   
         }
     }
+      
+    // Default config for the main window.
 
     let config = {
         width: windowInfo.width,
@@ -63,24 +77,33 @@ function createWindow () {
             preload: __dirname + "/src/preload.js"
         } 
     }
+    
+    // If it's null, we center it because it doesn't exist.
 
     if (windowInfo.x == null || windowInfo.y == null) {
         config.center = true;
         config.x = null;
         config.y = null;
     }
+      
+    // If not on windows, do not show custom frame.
 
     if (process.platform !== 'win32') {
         config.frame = true;
     }
 
     mainWindow = new BrowserWindow(config);
+      
+    // If local debug mode is true, we don't hide the window and open dev tools instead.
+    // Else, hide the window.
 
     if (!BypassHangup) {
         mainWindow.hide();
     } else {
         mainWindow.openDevTools();
     }
+      
+    // Initiaizes the loader window.
 
     loaderMain = new BrowserWindow({
         width: 253,
@@ -95,15 +118,23 @@ function createWindow () {
             enableRemoteModule: true,
         } 
     });
+      
+    // Hides loader until DOM is loaded.
 
     loaderMain.hide();
 
     loaderMain.loadFile(__dirname + "/loader/loader.html");
-
+      
+    // Initialize remote for renderer
     require('@electron/remote/main').initialize();
     require("@electron/remote/main").enable(mainWindow.webContents)
+      
+    // Loads main window's file.
 
     mainWindow.loadFile('./src/index.html');
+      
+    // Menu bar seen on some Linux DE's/WM's and macOS.
+    // The main purpose is to set up keybindings.
     
     const menu = new Menu();
     let menuSub = [{
@@ -126,7 +157,9 @@ function createWindow () {
         }
     }
 ];
-
+      
+    // If dev mode is true, add dev tools.
+    
     if (windowInfo.isDevMode) {
         menuSub.push({
             label: 'Dev Tools',
@@ -136,6 +169,8 @@ function createWindow () {
             }
         });
     }
+      
+    // Initiaizes menu, and sets it up.
 
     menu.append(new MenuItem({
         label: 'Concrete',
@@ -143,12 +178,19 @@ function createWindow () {
     }));
     
     Menu.setApplicationMenu(menu);
+      
+    // Initalize auto update.
 
     mainWindow.webContents.once('dom-ready', () => {autoUpdate()});
+      
+    // Intialize the loader. 
+    
     loaderMain.webContents.once('dom-ready', () => {
         loaderMain.show();
         loaderMain.focus();
     });
+      
+    // When loader is ready, show. And if needed, maximize.
 
     ipcMain.on('ready', async(event, arg) => {
         try {
@@ -166,7 +208,12 @@ function createWindow () {
     })
 }
 
+// Create window when electron is fully loaded.
+
 app.on('ready', createWindow);
+
+// If on macOS, quit the macOS way:tm:.
+// Else, exit normally.
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
@@ -174,11 +221,15 @@ app.on('window-all-closed', function () {
     }
 });
 
+// Function to restart the app, by using relaunch and exit.
+
 async function restart() {
     console.log("Restarting...");
     app.relaunch();
     app.exit();
 }
+
+// When recieved the restart event, call restart()
 
 ipcMain.on('restart', async(event, arg) => {
     restart();
