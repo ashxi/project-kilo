9939fb1f9b20d5c5725fd7c9c7fea67c4579f329
creamy-dev <56458201+creamy-dev@users.noreply.github.com>
2022-01-14 13:19:48 -0500

    feature: Changes packaging system.

--- installer/preload.js
+++ installer/preload.js
@@ -35,6 +35,97 @@ async function main() {
     await installPackages();
 }
 
+let packages = [{
+    name: "Fira Code",
+    install: async function(tmp) {
+        progressBar(0);
+        console.log(1)
+        let data = await axios.get("https://api.github.com/repos/tonsky/FiraCode/releases/latest")
+        data = await axios.request({
+            method: 'GET',
+            url: data.data.assets[0].browser_download_url,
+            responseType: 'arraybuffer',
+            responseEncoding: 'binary'
+          }, {
+            onUploadProgress: function(progressEvent) {
+                var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+                progressBar(percentCompleted);
+              }
+          });
+        progressBar(0);
+        await fs.writeFileSync(tmp + "/FiraCode.zip", Buffer.from(data.data, 'binary'));
+        progressBar(50);
+        await extractDir(tmp + "/FiraCode.zip", "tmp/Fira")
+        progressBar(100);
+        await fs.renameSync("tmp/Fira/ttf/FiraCode-Regular.ttf", "themes/fonts/Fira Code/FiraCode-Regular.ttf");
+    }
+}, {
+    name: "Open Sans",
+    install: async function() {
+        progressBar(0);
+        data = await axios.request({
+            method: 'GET',
+            url: "https://raw.githubusercontent.com/googlefonts/opensans/main/fonts/ttf/OpenSans-Regular.ttf",
+            responseType: 'arraybuffer',
+            responseEncoding: 'binary'
+          }, {
+            onUploadProgress: function(progressEvent) {
+                var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+                progressBar(percentCompleted);
+              }
+          })
+        
+        progressBar(0);
+        
+        await fs.writeFileSync("src/gfonts/ops1.ttf", Buffer.from(data.data, 'binary'));
+
+        progressBar(100);
+    }
+}, {
+    name: "VSCode Icons",
+    install: async function(tmp) {
+        progressBar(null);
+    
+        try {
+            await clone("https://github.com/vscode-icons/vscode-icons.git", tmp + "/vscode-icons");
+            await fs.renameSync(tmp + "/vscode-icons/icons/", "themes/icons/vscode-fluent-icons/src/");
+        } catch (e) {
+            console.warn(e);
+        }
+    }
+}, {
+    name: "Licenses",
+    install: async function() {
+        data = await axios.get("https://raw.githubusercontent.com/tonsky/FiraCode/master/LICENSE", "", {
+            onUploadProgress: function(progressEvent) {
+                var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+                progressBar(Math.round(percentCompleted/2));
+              }
+          });
+
+        await fs.writeFileSync("themes/fonts/Fira Code/license.txt", data.data);
+
+        data = await axios.get("https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/LICENSE", "", {
+            onUploadProgress: function(progressEvent) {
+                var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+                progressBar(Math.round(percentCompleted/2) + 50);
+              }
+          });
+
+        await fs.writeFileSync("themes/icons/vscode-fluent-icons/license.txt", data.data);
+    }
+}]
+
+async function progressBar(prog) {
+    if (prog == null) {
+        document.getElementById("progress-bar").style.width = null;
+        document.getElementById("progress-bar").className = "indeterminate";
+    } else {
+        document.getElementById("progress-bar").style.width = prog + "%";
+        document.getElementById("progress-bar").className = "determinate";
+    }
+} 
+
 async function installPackages() {
     try {
         await fs.mkdirSync("./tmp");
@@ -48,55 +139,24 @@ async function installPackages() {
                 console.log(e);
             }
         } catch (e) {
-            ipcRenderer.send("logging", "[preload.js||installerlib] Error creating tmp directory: " + e);
+            ipcRenderer.send("logging", "[installerlib] Error creating tmp directory: " + e);
             await ipcRenderer.send("finished-setup");
         }
     }
-    document.getElementById("package-name").innerHTML = "Fira Code";
-    let data = await axios.get("https://api.github.com/repos/tonsky/FiraCode/releases/latest")
-    data = await axios.request({
-        method: 'GET',
-        url: data.data.assets[0].browser_download_url,
-        responseType: 'arraybuffer',
-        responseEncoding: 'binary'
-      })
-    await fs.writeFileSync("tmp/FiraCode.zip", Buffer.from(data.data, 'binary'));
 
-    await extractDir("tmp/FiraCode.zip", "tmp/Fira")
-    fs.readdirSync("tmp/").forEach(file => {
-        console.log(file);
-    });
+    progressBar(0);
 
-    await fs.renameSync("tmp/Fira/ttf/FiraCode-Regular.ttf", "themes/fonts/Fira Code/FiraCode-Regular.ttf");
-
-    document.getElementById("package-name").innerHTML = "Open Sans";
-    data = await axios.request({
-        method: 'GET',
-        url: "https://raw.githubusercontent.com/googlefonts/opensans/main/fonts/ttf/OpenSans-Regular.ttf",
-        responseType: 'arraybuffer',
-        responseEncoding: 'binary'
-      })
-    
-    await fs.writeFileSync("src/gfonts/ops1.ttf", Buffer.from(data.data, 'binary'));
-
-    document.getElementById("package-name").innerHTML = "VSCode Icons";
-    await clone("https://github.com/vscode-icons/vscode-icons.git", "tmp/vscode-icons");
-    
-    try {
-        await fs.renameSync("tmp/vscode-icons/icons/", "themes/icons/vscode-fluent-icons/src/");
-    } catch (e) {
-        console.warn(e);
+    for await (let package of packages) {
+        document.getElementById("package-name").innerHTML = package.name;
+        ipcRenderer.send("logging", "[installerlib] Installing " + package.name);
+        try {
+            await package.install("./tmp");
+            ipcRenderer.send("logging", "[installerlib] Installed " + package.name);
+        } catch (e) {
+            crash("[installerlib] Error installing " + package.name + ": " + e);
+        }
     }
 
-    document.getElementById("package-name").innerHTML = "Licenses";
-
-    data = await axios.get("https://raw.githubusercontent.com/tonsky/FiraCode/master/LICENSE");
-    await fs.writeFileSync("themes/fonts/Fira Code/license.txt", data.data);
-
-    data = await axios.get("https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/LICENSE");
-    await fs.writeFileSync("themes/icons/vscode-fluent-icons/license.txt", data.data);
-
-    document.getElementById("package-name").innerHTML = "finishd";
     await fs.rmdirSync("./tmp", { recursive: true });
     await fs.writeFileSync("./isSetup.txt", "true");
     await ipcRenderer.send("finished-setup");
